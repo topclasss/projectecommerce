09a38588ed571e20250eb16cd9758384d5dc2735
Samir Nori <primemotiondesigns@gmail.com>
2023-11-20 16:52:55 -0500

    "Navbar and App.js Tweaks"

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./Reused/GlobalStyles";
 import LoginPage from './routes/LoginPage/LoginPage';
 import RegisterPage from './routes/RegisterPage/RegisterPage';
@@ -8,54 +8,32 @@ import ByCategoryPage from "./routes/StorePage/ByCategoryPage"
 import DetailsPage from "./routes/StorePage/DetailsPage"
 import { UserProvider, useUser } from './Reused/CustomerContext ';
 import CartPage from './routes/CartPage/CartPage';
-
-
-const Navbar = () => {
-  const { customer, logout } = useUser();
-
-
-  if (customer) {
-    // Customer is logged in, show Logout button
-    return (
-      <>
-        <button onClick={logout}>Logout</button>
-      </>
-    );
-  }
-
-
-  // Customer is not logged in, show Login button
-  return (
-    <>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-    </>
-  );
-};
+import Navbar from './Navbar';
 
 
 const App = () => {
-    return (
-      <BrowserRouter>
-        <GlobalStyle />
-        <UserProvider>
-        <Routes>
+  const [key, setKey] = useState(0);
+
+  const handleNavigation = () => {
+    setKey((prevKey) => prevKey + 1);
+  };
+
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <UserProvider>
+        <Navbar onNavigate={handleNavigation} />
+        <Routes key={key}>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/store" element={<StorePage/>} />
-          <Route path="/store/:categoryId" element={<ByCategoryPage/>} />
-          <Route path="/product/:productId" element={<DetailsPage/>} />
+          <Route path="/store" element={<StorePage />} />
+          <Route path="/store/:categoryId" element={<ByCategoryPage />} />
+          <Route path="/product/:productId" element={<DetailsPage />} />
           <Route path="/cart" element={<CartPage />} />
-          </Routes>
-          <Link to="/store">Home</Link>
-          <br/>
-          <Link to="/cart">Cart Page</Link>
-          <br/>
-          <Navbar />
-          </UserProvider>
-      </BrowserRouter>
-    );
-  };
- 
-  export default App;
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
